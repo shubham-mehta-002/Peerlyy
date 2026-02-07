858023a4897fe599ee095ac632e27a77f937f0a3
shubham-mehta-002 <mehta.shubham002@gmail.com>
2026-02-07 19:21:30 +0530

    project setup done

---
+++ server/script.ts
@@ -0,0 +1,17 @@
+import { prisma } from "./lib/prisma"
+
+
+async function main() {
+    const users = await prisma.user.findMany()
+    console.log({ users })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
